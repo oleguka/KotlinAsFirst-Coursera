22541de3339a5f222a2c2a83e5c94d872483db29
oleguka <olrgolrg@yandex.ru>
2018-10-06 14:53:11 +0500

    Часть задач первого урока

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -48,8 +48,8 @@ fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
  * Пример главной функции
  */
 fun main(args: Array<String>) {
-    val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
-    println("Root product: $x1x2")
+    val result = sqr(8)
+    println("result = $result")
 }
 
 /**
@@ -58,7 +58,7 @@ fun main(args: Array<String>) {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int = seconds + minutes * 60 + hours * 3600
 
 /**
  * Тривиальная
@@ -67,7 +67,12 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
+    val s = sagenes * 48
+    val a = arshins * 16
+    val result = (s + a + vershoks) * 4.445 / 100
+    return result
+}
 
 /**
  * Тривиальная
@@ -83,7 +88,11 @@ fun angleInRadian(grad: Int, min: Int, sec: Int): Double = TODO()
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double {
+    val summ = (x2 - x1) * (x2 - x1) + (y2 - y1)*(y2 - y1)
+    val result = sqrt(summ)
+    return result
+}
 
 /**
  * Простая
@@ -100,7 +109,12 @@ fun thirdDigit(number: Int): Int = TODO()
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int {
+    val hoursA = abs(hoursArrive - 1)
+    val minutesA = minutesArrive + 60
+    val result = (hoursA - hoursDepart) * 60 + (minutesA - minutesDepart)
+    return result
+}
 
 /**
  * Простая
